482e0019719eb4c87edb188ea2f698724812ab3f
Ian McNally <imcnally@stridenyc.com>
2015-02-24 16:35:40 -0500

    use babelify

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
-var to5ify = require('6to5ify');
+var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 
 gulp.task('compile', function(){
   return browserify('./src/app.jsx')
-    .transform(to5ify)
+    .transform(babelify)
     .bundle()
     .pipe(source('main.js'))
     .pipe(gulp.dest('./dist/javascript'));
